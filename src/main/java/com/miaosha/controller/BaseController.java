f8e4a5ba857bc135942a14bb8f34a9368cb6eb63
huangdili <m17602506902@163.com>
2019-07-09 22:10:36 +0800

    提交

--- src/main/java/com/miaosha/controller/BaseController.java
+++ src/main/java/com/miaosha/controller/BaseController.java
@@ -1,4 +1,33 @@
 package com.miaosha.controller;
 
+import com.miaosha.error.BussinessException;
+import com.miaosha.error.EmBussinessError;
+import com.miaosha.response.CommentReturnType;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
 public class BaseController {
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Object handlerException(HttpServletRequest request, Exception exception){
+        CommentReturnType commentReturnType = new CommentReturnType();
+        Map<String, Object> responseData = new HashMap<String, Object>();
+        if(exception instanceof BussinessException) {
+            BussinessException bussinessException = (BussinessException) exception;
+            responseData.put("errCode", bussinessException.getErrCode());
+            responseData.put("errMsg", bussinessException.getErrMsg());
+        }else{
+            responseData.put("errCode", EmBussinessError.UNKNOWN_ERROR.getErrCode());
+            responseData.put("errMsg", EmBussinessError.UNKNOWN_ERROR.getErrMsg());
+        }
+        return commentReturnType.create(responseData,"fail");
+
+    }
 }
--- src/main/java/com/miaosha/controller/UserController.java
+++ src/main/java/com/miaosha/controller/UserController.java
@@ -1,25 +1,36 @@
 package com.miaosha.controller;
 
 import com.miaosha.controller.viewobject.UserVO;
+import com.miaosha.error.BussinessException;
+import com.miaosha.error.EmBussinessError;
+import com.miaosha.response.CommentReturnType;
 import com.miaosha.service.UserService;
 import com.miaosha.service.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller("user")
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseController{
     @Autowired UserService userService;
 
     @RequestMapping("/get")
     @ResponseBody
-    public UserVO getUser(@RequestParam(name = "id")Integer id){
+    public CommentReturnType getUser(@RequestParam(name = "id")Integer id) throws BussinessException {
         //调用sevice服务
         UserVO userVO = userService.getUserById(id);
-        return userVO;
+        if(userVO == null){
+            throw new BussinessException(EmBussinessError.USER_NOT_EXIST);
+        }
+
+        return CommentReturnType.create(userVO);
 
     }
+
 }
